e147acf16fd10dc2aeacaf6783b42814eb586efc
David Grenier <davidgrenier@gmail.com>
2023-04-18 13:34:37 -0400

    Adds a nasty header file

--- gramSchmidt.cpp
+++ gramSchmidt.cpp
@@ -1,62 +1,12 @@
 #include <iostream>
 #include <numbers>
 #include <Eigen/Dense>
-
-using namespace Eigen;
-using namespace std;
-using std::numbers::pi;
-
-constexpr auto eps = numeric_limits<double>::epsilon();
+#include "myGram.h"
 
 template<int m, int n>
 using M = Matrix<double, m, n>;
 template<int m>
 using V = Vector<double, m>;
-
-template<int m, int n>
-pair<V<m>, V<n>> classic(const V<m>& aj, const M<m,n>& q, int j) {
-    V<m> qj = aj;
-    V<n> rj = V<n>::Zero();
-    for (int i = 0; i < j; i++) {
-        rj(i) = q.col(i).dot(aj);
-        qj -= rj(i)*q.col(i);
-    }
-    rj(j) = qj.norm();
-    qj.normalize();
-
-    return make_pair(qj,rj);
-}
-
-template<int m, int n>
-pair<V<m>, V<n>> modified(V<m> qj, const M<m,n>& q, int j) {
-    V<n> rj = V<n>::Zero();
-    for (int i = 0; i < j; i++) {
-        rj(i) = q.col(i).dot(qj);
-        qj -= rj(i)*q.col(i);
-    }
-    rj(j) = qj.norm();
-    qj.normalize();
-
-    return make_pair(qj,rj);
-}
-
-template<int m, int n, typename Ortho>
-pair<M<m,n>, M<n,n>> QR(const M<m,n>& a, const Ortho& o) {
-    M<m,n> q = M<m,n>::Zero();
-    M<n,n> r = M<n,n>::Zero();
-
-    for (int j = 0; j < n; j++) {
-        auto qrj = o(a.col(j), q, j);
-        r.col(j) = qrj.second;
-        for (int i = 0; qrj.second(j) <= 2*eps && i < n; i++) {
-            r(j,j) = 0;
-            qrj = o(Vector<double, m>::Unit(i), q, j);
-        }
-        q.col(j) = qrj.first;
-    }
-
-    return make_pair(q,r);
-}
 
 template<int m, int n>
 void showQR(const M<m,n> a, const pair<M<m,n>, M<n,n>>& qr) {
@@ -77,6 +27,6 @@ int main() {
     M<m,n> a = M<m,n>::Random();
     cout << "A =\n" << a << "\n\n";
 
-    showQR(a, QR(a, classic<m,n>));
-    showQR(a, QR(a, modified<m,n>));
+    showQR(a, QRc(a));
+    showQR(a, QRm(a));
 }
